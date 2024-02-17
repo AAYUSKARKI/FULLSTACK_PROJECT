83f27be7b78837a26b81e5438059d04eeaa59918
AAYUS KARKI <alexkarki2060@gmail.com>
2024-02-17 15:47:09 +0545

    middleware added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary} from "cloudinary"
+import { Console } from "console";
+import fs from "fs" //file system
+
+      
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret:  process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try{
+if(!localFilePath) return null
+//uploade the file on cloudinary
+const response = await cloudinary.uploader.upload(localFilePath,{
+    resource_type: "auto"
+})
+
+//file upload sucessfully
+console.log("file is upload on cloudinary",response.url);
+return response;
+    }
+    catch (error){
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file
+        Console.log(error);
+        return null
+    }
+}
+
+
+
+
+
+
+  export {uploadOnCloudinary}
